32a7e89ec9e3189257c1b6cc27ea94ca402299e3
wei deng <wdeng@mozilla.com>
2012-10-30 13:03:17 +0800

    modify nightly build script

--- nighly.php
+++ nighly.php
@@ -1,7 +1,45 @@
 #!/bin/bash
 
-check_repositories(){
-	echo $1
+GECKO_URL="git://github.com/mozilla/releases-mozilla-central"
+GAIA_URL="git://github.com/mozilla-b2g/gaia"
+
+#$1 gecko or gaia
+#$2 remote server url
+fetch_repositories() {
+	cd $1
+	git remote add nighly $2
+	git fetch nighly
+	git checkout -b nighly nighly/master
+	cd ..
 }
 
-check_repositories GECKO
+#$1 gecko or gaia
+update_repositories() {
+	cd $1
+	if [ $1 = "gecko" ]; then
+		git reset --hard
+		git pull
+		git apply *.patch
+	else
+		git pull
+	fi
+	cd ..
+}
+
+#$1 gecko or gaia
+#$2 remote server url
+check_repositories() {
+	if [ -f ".$1url" ]; then
+		update_repositories $1
+	else
+		fetch_repositories $1 $2
+		touch .$1url
+	fi
+#	echo $1
+}
+
+check_repositories gecko $GECKO_URL
+check_repositories gaia $GAIA_URL
+
+
+#. build.sh
